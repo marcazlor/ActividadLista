9f51e1d44ef4191afadca991292c42b2b2112904
marcazlor <marc.azlor@estudiantat.upc.edu>
2021-10-22 20:13:52 +0200

    Initial commit

---
+++ app/src/main/java/edu/upc/dsa/lista2/ListElement.java
@@ -0,0 +1,47 @@
+package edu.upc.dsa.lista2;
+
+public class ListElement {
+    public String color;
+    public String name;
+    public String city;
+    public String status;
+
+    public ListElement(String color, String name, String ciudad, String status) {
+        this.color = color;
+        this.name = name;
+        this.city = ciudad;
+        this.status = status;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
